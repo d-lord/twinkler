26c46bd0bb97290a2ef9ddadf97e3630c8cb05b9
Will Toohey <will@mon.im>
2022-07-16 18:33:50 +1000

    Add the blinkenlights

--- src/Show.tsx
+++ src/Show.tsx
@@ -34,6 +34,24 @@ export default class Show extends React.Component<ShowProps, ShowState> {
         this.state = {timerId: null, formattedCode: null, allClasses: []}
     }
 
+    // https://stackoverflow.com/a/12646864
+    shuffleArray(array: string[]) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        return array;
+    }
+
+    addStyle(styleString: string) {
+        const style = document.createElement('style');
+        style.textContent = styleString;
+        document.head.append(style);
+      }
+
     componentDidMount() {
         console.log(`Show.componentDidMount (state.formattedCode: ${this.state.formattedCode})`)
         const pre = document.createElement('pre')
@@ -79,10 +97,61 @@ export default class Show extends React.Component<ShowProps, ShowState> {
             }
         }
 
-        const meaningfulClasses = Array.from(allClasses).filter((className) => {
+        let meaningfulClasses = Array.from(allClasses).filter((className) => {
             temporarySpanTag.className = className
             return window.getComputedStyle(temporarySpanTag).getPropertyValue('color') !== 'inherit'
         })
+
+        const colours = ["#f6f3e8", "#656565", "#ffffff", "#f6f3e8", "#f6f3e8", "#343434", "#857b6f", "#384048", "#a0a8b0", "#f6f3e8", "#857b6f", "#e5786d", "#ddaa6f", "#ddaa6f", "#e5786d", "#99968b", "#e5786d", "#cae682", "#8ac6f2", "#95e454", "#92a65e", "#cae682", "#ccaa8f", "#8ac6f2", "#e5786d", "#f6f3e8", "#e7f6da", "#95e454", "#95e454", "#cae682", "#cae682", "#ccaa8f", "#ccaa8f", "#99968b", "#99968b", "#cae682", "#cae682", "#99968b", "#95e454", "#95e454", "#cae682", "#cae682", "#ccaa8f", "#ccaa8f", "#99968b", "#8ac6f2", "#95e454", "#cae682", "#8ac6f2", "#cae682", "#8ac6f2", "#95e454", "#95e454", "#cae682", "#cae682", "#99968b", "#e5786d", "#e5786d", "#95e454", "#2cae682", "#8ac6f2", "#ccaa8f", "#f6f3e8"];
+        const secPerColour = 0.5;
+        const subPercent = 1 / colours.length;
+        const holdPercent = 0.15 * subPercent;
+        const fullLen = secPerColour * colours.length;
+        const extraStyle = [];
+
+        // edge case for the root
+        meaningfulClasses = ['hljs', ...meaningfulClasses];
+
+        for (const className of meaningfulClasses) {
+            const newClass = `
+                .${className}-anim {
+                    animation-name: ${className}-keyframes;
+                    animation-duration: ${fullLen}s;
+                    animation-iteration-count: infinite;
+                }
+            `;
+            const keyframes = [
+                `@keyframes ${className}-keyframes {`,
+            ];
+
+            const thisColours = this.shuffleArray([...colours]);
+            thisColours.push(thisColours[0]);
+
+            for (const [i, colour] of thisColours.entries()) {
+                const subPercentStart = i * subPercent;
+                const nextPercent = (i+1) * subPercent;
+                const start = i == 0 ? 'from' : `${subPercentStart*100}%`;
+                const first = subPercentStart + holdPercent;
+                const last = nextPercent - holdPercent;
+                const end = i == (thisColours.length - 1) ? 'to' : `${nextPercent*100}%`;
+                keyframes.push(`${start} { color: ${colour}; }`)
+            }
+
+            keyframes.push('}');
+
+            extraStyle.push(newClass);
+            extraStyle.push(...keyframes);
+
+            for (const el of pre.querySelectorAll(`.${className}`)) {
+                el.classList.add(`${className}-anim`);
+            }
+            if (pre.classList.contains(className)) {
+                pre.classList.add(`${className}-anim`);
+            }
+        }
+
+        this.addStyle(extraStyle.join('\n'));
+
         // with this we can find how many CSS classes we need (hence how many colours) and implement change() to start swapping them
 
         /* on mount: declare --span-tag: #999cba; */
